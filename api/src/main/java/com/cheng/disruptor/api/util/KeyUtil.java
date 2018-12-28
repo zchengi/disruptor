076475b494a27f2d69e2f2c89d7fbda03743091e
cheng <792702352@qq.com>
2018-12-28 12:39:38 +0800

    :sparkles: 带有时间顺序的ID生成策略

---
+++ api/src/main/java/com/cheng/disruptor/api/util/KeyUtil.java
@@ -0,0 +1,22 @@
+package com.cheng.disruptor.api.util;
+
+import com.fasterxml.uuid.EthernetAddress;
+import com.fasterxml.uuid.Generators;
+import com.fasterxml.uuid.impl.TimeBasedGenerator;
+
+/**
+ * @author cheng
+ *         2018/12/28 12:35
+ */
+public class KeyUtil {
+
+    public static String generatorUUID() {
+        TimeBasedGenerator timeBasedGenerator = Generators.timeBasedGenerator(EthernetAddress.fromInterface());
+        return timeBasedGenerator.generate().toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(KeyUtil.generatorUUID());
+        System.out.println(KeyUtil.generatorUUID());
+    }
+}
